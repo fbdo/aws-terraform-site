7d7cf2a39c2bd39b38686e3984a908b55d1d5a8e
Fabio Oliveira <fabio.braga@gmail.com>
2019-01-04 22:51:45 +0100

    Add S3 bucket and DynamoDB table for TF state management

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -1,17 +1,6 @@
 provider "aws" {
   region = "eu-central-1"
-  profile = "fabiooliveira_website"
   version = "~> 1.27"
-}
-
-provider "random" {
-    version = "= 1.3.1"
-}
-
-resource "random_string" "website-s3-state" {
-    lenght = 8
-    special = false
-    upper = false
 }
 
 provider "local" {
@@ -25,7 +14,7 @@ provider "template" {
 module "website-tfstate" {
   source = "git::https://github.com/confluentinc/terraform-state"
   env = "prod"
-  s3_bucket = "me.fabiooliveira.website.tfstate.${random_string.website.result}"
+  s3_bucket = "me.fabiooliveira.website.tfstate"
   s3_bucket_name = "fabiooliveira.me website Terraform state"
   dynamodb_table = "fabiooliveira-website-tfstate-lock"
 }
