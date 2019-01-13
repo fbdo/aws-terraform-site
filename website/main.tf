0807e94d6ae6e4cd118a215b43765a8a33cd4ef7
Fabio Oliveira <fabio.braga@gmail.com>
2019-01-13 15:09:52 +0100

    Change deployment to use a CDN

--- website/main.tf
+++ website/main.tf
@@ -12,11 +12,12 @@ terraform {
   }
 }
 
-module "website_with_cname" {
-  source         = "git::https://github.com/cloudposse/terraform-aws-s3-website.git?ref=master"
-  namespace      = "website"
-  stage          = "prod"
-  name           = "fabiooliveira"
-  hostname       = "www.fabiooliveira.me"
-  parent_zone_id = "Z2T01GNL4E6WCM"
+
+module "cdn" {
+  source           = "git::https://github.com/cloudposse/terraform-aws-cloudfront-s3-cdn.git?ref=master"
+  namespace        = "website"
+  stage            = "prod"
+  name             = "fabiooliveira"
+  aliases          = ["www.fabiooliveira.me","fabiooliveira.me"]
+  parent_zone_id   = "Z2T01GNL4E6WCM"
 }
