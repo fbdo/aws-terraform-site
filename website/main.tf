56e4d1c620bbf8a952a9caa0141aeb3ada07e2f0
Fabio Oliveira <fabio.oliveira@bmw.de>
2019-01-15 10:13:05 +0100

    Add lambda function to rewrite requests ending in / to /index.html

--- website/main.tf
+++ website/main.tf
@@ -13,56 +13,60 @@ terraform {
 }
 
 data "archive_file" "rewrite" {
-  type = "zip"
+  type        = "zip"
   output_path = "${path.module}/.zip/rewrite.zip"
+
   source {
     filename = "lambda_index_url_rewrite.js"
-    content = "${file("files/lambda_index_url_rewrite.js")}"
+    content  = "${file("files/lambda_index_url_rewrite.js")}"
   }
 }
 
 resource "aws_lambda_function" "rewrite" {
-  function_name = "lambda_index_url_rewrite"
-  filename = "${data.archive_file.rewrite.output_path}"
+  function_name    = "lambda_index_url_rewrite"
+  filename         = "${data.archive_file.rewrite.output_path}"
   source_code_hash = "${data.archive_file.rewrite.output_base64sha256}"
-  role = "${aws_iam_role.main.arn}"
-  runtime = "nodejs6.10"
-  handler = "index.handler"
-  memory_size = 128
-  timeout = 3
-  publish = true
+  role             = "${aws_iam_role.main.arn}"
+  runtime          = "nodejs6.10"
+  handler          = "index.handler"
+  memory_size      = 128
+  timeout          = 3
+  publish          = true
 }
 
 data "aws_iam_policy_document" "lambda" {
   statement {
     actions = ["sts:AssumeRole"]
+
     principals {
       type = "Service"
+
       identifiers = [
         "lambda.amazonaws.com",
-        "edgelambda.amazonaws.com"
+        "edgelambda.amazonaws.com",
       ]
     }
   }
 }
+
 resource "aws_iam_role" "main" {
-  name_prefix = "${var.app_name}"
+  name_prefix        = "${var.app_name}"
   assume_role_policy = "${data.aws_iam_policy_document.lambda.json}"
 }
 
 resource "aws_iam_role_policy_attachment" "basic" {
-  role = "${aws_iam_role.main.name}"
+  role       = "${aws_iam_role.main.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 module "cdn" {
-  source                 = "git::https://github.com/cloudposse/terraform-aws-cloudfront-s3-cdn.git?ref=master"
-  namespace              = "website"
-  stage                  = "prod"
-  name                   = "fabiooliveira"
-  aliases                = ["www.fabiooliveira.me", "fabiooliveira.me"]
-  parent_zone_id         = "Z2T01GNL4E6WCM"
-  compress               = true
-  viewer_protocol_policy = "allow-all"
+  source                      = "git::https://github.com/cloudposse/terraform-aws-cloudfront-s3-cdn.git?ref=master"
+  namespace                   = "website"
+  stage                       = "prod"
+  name                        = "fabiooliveira"
+  aliases                     = ["www.fabiooliveira.me", "fabiooliveira.me"]
+  parent_zone_id              = "Z2T01GNL4E6WCM"
+  compress                    = true
+  viewer_protocol_policy      = "allow-all"
   lambda_function_association = ["${aws_lambda_function.rewrite.qualified_arn}"]
 }
