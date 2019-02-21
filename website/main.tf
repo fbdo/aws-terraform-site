e87b8f36aa8a51189541f720df602a5181fe9744
Fabio Oliveira <fabio.braga@gmail.com>
2019-02-21 20:27:27 +0100

    Moving certificate to us-east-1

--- website/main.tf
+++ website/main.tf
@@ -62,6 +62,7 @@ resource "aws_iam_role_policy_attachment" "basic" {
 }
 
 resource "aws_acm_certificate" "cert" {
+  provider         = "aws.use1"
   domain_name       = "fabiooliveira.me"
   validation_method = "DNS"
 
@@ -83,7 +84,7 @@ module "cdn" {
   parent_zone_id         = "Z2T01GNL4E6WCM"
   compress               = true
   viewer_protocol_policy = "redirect-to-https"
-  acm_certificate_arn    = "${aws_acm_certificate.cert.arn}"
+  #acm_certificate_arn    = "${aws_acm_certificate.cert.arn}"
 
   lambda_function_association = [{
     lambda_arn = "arn:aws:lambda:us-east-1:144289250204:function:lambda_index_url_rewrite:3"
